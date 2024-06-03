740d1f01567849a50000a9be06cb7db91a4996f4
Laurent Vrevin <laurent.vrevin@hotmail.fr>
2024-06-03 14:32:26 +0200

    initial commit

---
+++ src/main/java/com/example/resaresto/ResarestoApplication.java
@@ -0,0 +1,13 @@
+package com.example.resaresto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResarestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResarestoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/resaresto/ResarestoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resaresto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResarestoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
